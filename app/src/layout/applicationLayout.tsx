822e20cae8f32bd8d634f63766d2fb31fc3a8e7c
Sam woodcock <160071790+sam-1f100@users.noreply.github.com>
2024-06-05 07:17:37 +0000

    logout

--- app/src/layout/applicationLayout.tsx
+++ app/src/layout/applicationLayout.tsx
@@ -297,7 +297,7 @@ const ApplicationLayout: React.FC<Props> = ({children, host, channel, isChanelPa
                                                 <Menu.Item>
                                                     {({active}) => (
                                                         <a
-                                                            href="#"
+                                                            href="/auth"
                                                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                         >
                                                             Sign out
