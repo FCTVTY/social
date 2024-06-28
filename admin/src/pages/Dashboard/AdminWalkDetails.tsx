9e26301e54f981ef9c7a2dd0f3a259543abdc301
Sam woodcock <160071790+sam-1f100@users.noreply.github.com>
2024-06-28 14:20:34 +0100

    Update AdminWalkDetails.tsx

--- admin/src/pages/Dashboard/AdminWalkDetails.tsx
+++ admin/src/pages/Dashboard/AdminWalkDetails.tsx
@@ -97,7 +97,7 @@ export const DashboardPage: FC = () => {
                 <h3 className="text-base font-semibold leading-7 ">Community Information </h3>
                 <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-400">{community.name}</p>
                  <a href={`http://${community.url}.app.bhivecommunity.co.uk`}
-                       className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold pt-10  shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 text-white"
+                       className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold mt-10  shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 text-white"
                     >
                      visit Community
                     </a>
