915d056144e67efbd876a729ccb2c1d3943eec0e
Sam woodcock <160071790+sam-1f100@users.noreply.github.com>
2024-07-29 23:59:28 +0100

    Update applicationLayout.tsx

--- app/src/layout/applicationLayout.tsx
+++ app/src/layout/applicationLayout.tsx
@@ -296,7 +296,7 @@ const ApplicationLayout: React.FC<Props> = ({children, host, channel, isChanelPa
                                 <div
                                   className="absolute inset-y-0 right-0 justify-content-end flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
-<ThemeSwitch></ThemeSwitch>
+
 
                                     {/* Profile dropdown */}
                                     <Menu as="div" className="relative ml-3">
