0b2483b1906dc8e2203aa1071fd2449a4f43dabe
Sam Woodcock <sam@1f100.net>
2024-09-06 11:02:33 +0100

    fix issue on ffed

--- app/src/Pages/home/Feed.tsx
+++ app/src/Pages/home/Feed.tsx
@@ -26,7 +26,7 @@ import {
   PlusIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/16/solid";
-import { BadgeCheck } from "lucide-react";
+import { BadgeCheck, CakeIcon, MessageCircle, ScrollText } from "lucide-react";
 
 interface HomeProps {
   host?: string;
@@ -479,38 +479,18 @@ export default function Feed({ host, channel, roles, setRoles }: HomeProps) {
                     </div>
                   )}
                 </div>
-                <ul className="py-4 mt-2 text-gray-700 flex items-center justify-around hidden">
-                  <li className="flex flex-col items-center justify-around">
-                    <svg
-                      className="w-4 fill-current text-blue-900"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                    </svg>
-                    <div>2k</div>
-                  </li>
-                  <li className="flex flex-col items-center justify-between">
-                    <svg
-                      className="w-4 fill-current text-blue-900"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M7 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1c2.15 0 4.2.4 6.1 1.09L12 16h-1.25L10 20H4l-.75-4H2L.9 10.09A17.93 17.93 0 0 1 7 9zm8.31.17c1.32.18 2.59.48 3.8.92L18 16h-1.25L16 20h-3.96l.37-2h1.25l1.65-8.83zM13 0a4 4 0 1 1-1.33 7.76 5.96 5.96 0 0 0 0-7.52C12.1.1 12.53 0 13 0z" />
-                    </svg>
-                    <div>10k</div>
-                  </li>
-                  <li className="flex flex-col items-center justify-around">
-                    <svg
-                      className="w-4 fill-current text-blue-900"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
-                    </svg>
-                    <div>15</div>
-                  </li>
-                </ul>
+                {profile && (
+                  <ul className="py-4 mt-2 text-gray-700 flex items-center justify-around">
+                    <li className="flex flex-col items-center justify-around">
+                      <ScrollText />
+                      <div>{profile?.posts.length}</div>
+                    </li>
+                    <li className="flex flex-col items-center justify-between">
+                      <MessageCircle />
+                      <div>{profile?.commentCount}</div>
+                    </li>
+                  </ul>
+                )}
                 <div className="p-4 border-t mx-8 mt-2 hidden">
                   <button className="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">
                     Follow
@@ -525,7 +505,7 @@ export default function Feed({ host, channel, roles, setRoles }: HomeProps) {
               <div className="rounded-xl border-2 border-gray-100 bg-white dark:bg-zinc-950 dark:border-zinc-800 p-3 mb-4 dark:text-white ">
                 <img
                   src={community && community.community?.logo}
-                  className="mx-auto h-20 py-1 dark:invert"
+                  className="mx-auto h-20 w-40 py-1 dark:invert object-contain"
                 />
 
                 <p className={`py-6`}>{community?.community?.desc}</p>
