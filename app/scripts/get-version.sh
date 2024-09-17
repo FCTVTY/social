c6b99f3c17cfac5fb9a49c2a977434f8ae6d9a83
Sam Woodcock <sam@1f100.net>
2024-09-17 12:00:06 +0100

    fix error

--- app/scripts/get-version.sh
+++ app/scripts/get-version.sh
@@ -1,4 +1,4 @@
 VERSION=$(grep -m 1 -o '"version": *"[^"]*"' ./package.json | awk -F'"' '{print $4}')
 DATE=$(date +%Y%m%d%H%M%S) # Format: YYYYMMDDHHMMSS
 APP_VERSION="$VERSION-$DATE"
-echo $APP_VERSIO
+echo $APP_VERSION
