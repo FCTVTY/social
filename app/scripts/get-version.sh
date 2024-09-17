d53fda8f96a18e8fe528e0101dae099b5cbf50dc
Sam Woodcock <sam@1f100.net>
2024-09-17 11:52:44 +0100

    use date

--- app/scripts/get-version.sh
+++ app/scripts/get-version.sh
@@ -1,9 +1,4 @@
-#
-# Calvin C. Chan calvincchan.com 2023-Oct-18
-# Get the version number from package.json and append the git commit count and hash.
-#
-VERSION=$(grep -m 1 -o '"version": *"[^"]*"' ./package.json | awk -F'"' '{print $4}');
-GIT_COMMIT_COUNT=$(git rev-list --count HEAD);
-GIT_COMMIT_HASH=$(git rev-parse --short HEAD);
-APP_VERSION="$VERSION-$GIT_COMMIT_COUNT-$GIT_COMMIT_HASH";
-echo $APP_VERSION
+VERSION=$(grep -m 1 -o '"version": *"[^"]*"' ./package.json | awk -F'"' '{print $4}')
+DATE=$(date +%Y%m%d%H%M%S) # Format: YYYYMMDDHHMMSS
+APP_VERSION="$VERSION-$DATE"
+echo $APP_VERSIO
