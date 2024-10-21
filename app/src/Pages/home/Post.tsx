fca8afafbe491fe475b3fa30d40a5ba532ef198c
Sam woodcock <160071790+sam-1f100@users.noreply.github.com>
2024-10-21 14:40:08 +0100

    fix image

--- app/src/Pages/home/Post.tsx
+++ app/src/Pages/home/Post.tsx
@@ -424,7 +424,7 @@ export default function PostView({ host, channel, post, roles }: HomeProps) {
             </div>
             <div className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white dark:border-gray-800 rounded-full overflow-hidden">
               <img
-                className="object-cover object-center h-32"
+                className="object-cover object-center h-32 w-32"
                 src={
                   ppost?.profile?.profilePicture ||
                   `https://eu.ui-avatars.com/api/?name=${ppost?.profile?.first_name}+${ppost?.profile?.last_name}&size=250`
